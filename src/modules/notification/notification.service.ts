7bc2354be9452b141a356831a4efa054f35115ff
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2025-06-27 18:43:15 +0400

    fix

--- src/modules/notification/notification.service.ts
+++ src/modules/notification/notification.service.ts
@@ -47,7 +47,7 @@ export class NotifyService {
         return this.sendUnActiveUsers();
     }
 
-    @Cron(CronExpression.EVERY_10_MINUTES, { timeZone: 'Europe/Moscow' })
+    @Cron(CronExpression.EVERY_5_MINUTES, { timeZone: 'Europe/Moscow' })
     sendNewLids() {
         console.log('sendNewLids');
         return this.autoSettingsService.startUsersAutomations();
